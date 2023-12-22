3b67619737c90a208ff7815598bd548e8d1f37d8
Egor251 <egorpyshny@gmail.com>
2023-12-22 18:47:14 +0300

    fix gitignore, add catalog service

---
+++ catalog_service/catalog_service/service.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+service = Flask(__name__)
+
+
+@service.route("/shop/api/v1/catalog/add")
+def add_product():
+    return "<p>add</p>"
+
+
+@service.route("/shop/api/v1/catalog/remove")
+def remove_product():
+    return "<p>remove</p>"
+
+
+@service.route("/shop/api/v1/catalog/list")
+def list_product():
+    return "<p>list</p>"
+
+
+service.run(host='192.168.0.104', port=5000)
