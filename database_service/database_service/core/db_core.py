fda261083c460d3b097048189a338ecd18a019f5
Egor251 <egorpyshny@gmail.com>
2024-02-17 19:27:58 +0300

    fix

--- database_service/database_service/core/db_core.py
+++ database_service/database_service/core/db_core.py
@@ -4,14 +4,13 @@ import json
 import logging
 from typing import Dict
 
-from sqlalchemy import select, delete, desc, Table, text
+from sqlalchemy import select, delete, desc, Table, func
 from sqlalchemy.schema import MetaData
 from sqlalchemy import AsyncAdaptedQueuePool
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from models import Base
 from models.author_book_models import Books, Authors, book_author_association
 from utils.LoggerFormater import CustomFormatter
 
@@ -68,7 +67,6 @@ class Core:
                         book_price=data["book_price"],
                         book_authors=authors_list
                     )
-                    # new_book.book_authors.append(authors_list)
                     my_session.add(new_book)
                     return json.dumps({"res":"completed","exception":""})
         except IntegrityError as e:
@@ -89,10 +87,19 @@ class Core:
         async with self.custom_session() as my_session:
             async with my_session.begin():
                 data = json.loads(body)["data"]
-                stmt = select(Books).where(Books.book_id == data["id"])
-                book = await my_session.execute(stmt)
-                stmt = select(Authors).join(book_author_association, book_author_association.author_id)
-
+                stmt = select(Books, func.concat(func.array_agg(func.concat(Authors.author_name, ' ', Authors.author_surname)), ', ').label('authors')).\
+                    join(book_author_association, Books.book_id == book_author_association.c["book_id"]).\
+                    join(Authors, Authors.author_id == book_author_association.c["author_id"]).\
+                    group_by(Books.book_id, Books.book_title).\
+                    where(Books.book_id == data["book_id"])
+                res = await my_session.execute(stmt)
+                records = res.fetchone()
+                book = records[0].__dict__
+                del book['_sa_instance_state']
+                authors = str(records[1]).replace('{','[').replace('}',']')[:-2]
+                book["book_id"] = str(book["book_id"])
+                book["book_authors"] = authors
+                return json.dumps({"res": book, "exception": ""})
 
     async def catalog_list_func(self, body) -> Dict[str, str]:
         try:
@@ -125,21 +132,26 @@ class Core:
                 return json.dumps({"res": "exception", "exception": f"no such column as {e.args[0]}, possible columns: {MaterializedView.c}"})
 
 
-# c = Core()
-# b = """{"data":{
-#         "book_title":"0",
-#         "book_amount":1,
-#         "book_description":"description",
-#         "book_price": 1.0,
-#         "fiter_field":"book_thgitle",
-#         "filter_direction":"ASC",
-#         "page":0,
-#         "book_authors":[
-#             {
-#                 "author_name":"a3",
-#                 "author_surname":"s3"
-#             }
-#         ]
-#     }}"""
-# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-# asyncio.run(c.catalog_list_func(b))
+c = Core()
+b = """{"data":{
+        "book_title":"1",
+        "book_amount":1,
+        "book_id":"893ce751-fea0-427c-a5c9-f99cce6a15d5",
+        "book_description":"description",
+        "book_price": 1.0,
+        "fiter_field":"book_thgitle",
+        "filter_direction":"ASC",
+        "page":0,
+        "book_authors":[
+            {
+                "author_name":"a3",
+                "author_surname":"s3"
+            },
+            {
+                "author_name":"a5",
+                "author_surname":"s5"
+            }
+        ]
+    }}"""
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(c.catalog_list_func(b))
